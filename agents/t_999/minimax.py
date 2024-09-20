be3d930bf4295e6729bd8d1a76310c85c865fc87
Rain-Poon <85403806+Rain-Poon@users.noreply.github.com>
2024-09-20 12:17:01 +1000

    Update minimax.py

--- agents/t_999/minimax.py
+++ agents/t_999/minimax.py
@@ -9,18 +9,30 @@ THINKTIME = 0.9
 NUM_PLAYERS = 2
 
 def ScoreState(state):
+    """
+    Description:
+    Get the score of the current state given that the round has not ended.
+
+    Args:
+    state (AzulState): The current state of the game.
+
+    Returns:
+    score_change (int): The score of the current state.
+    """
+
     score_inc = 0
+    # calculate the score for tiles moved to grid_state
     grid_state = deepcopy(state.grid_state)
 
-    # 1. Action tiles across from pattern lines to the wall grid
+    # even though the round has not ended
+    # move tiles from pattern lines to wall grid
     for i in range(state.GRID_SIZE):
         # Is the pattern line full? If not it persists in its current
         # state into the next round.
-        ori_score = score_inc
         if state.lines_number[i] == i+1:
             tc = state.lines_tile[i]
             col = int(state.grid_scheme[i][tc])
-            # print(tc, col)
+
             # Tile will be placed at position (i,col) in grid
             grid_state[i][col] = 1
 
@@ -68,33 +80,44 @@ def ScoreState(state):
             if above == 0 and below == 0 and left == 0 and right == 0:
                 score_inc += 1
 
-            # print(score_inc - ori_score)
-
     # Score penalties for tiles in floor line
     penalties = 0
     for i in range(len(state.floor)):
         penalties += state.floor[i]*state.FLOOR_SCORES[i]
-    # print(f'From ScoreState: {score_inc}, {penalties}')
-    # Agents cannot be assigned a negative score in any round.
+    # return negative score to show penalties
+    # should not be bounded by 0
     score_change = score_inc + penalties
     return score_change
 
-import time
-
 class myAgent:
     def __init__(self, _id):
-        self.id = _id  # Agent needs to remember its own id.
-        self.game_rule = GameRule(NUM_PLAYERS)  # Agent stores an instance of GameRule.
-        self.best_action = None
+        self.id = _id
+        self.game_rule = GameRule(NUM_PLAYERS)
 
-    def minimax(self, state, depth, maximizingPlayer, alpha, beta, start_time, think_time, current_agent_id):
-        # Check if time limit has been reached
-        if time.time() - start_time > think_time:
-            return None  # Timeout
+    def minimax(self, state, depth, maximizingPlayer, alpha, beta, start_time, current_agent_id):
+        """
+        Description:
+        Minimax algorithm with alpha-beta pruning to find the best action for the agent.
+
+        Args:
+        state (AzulState): The current state of the game.
+        depth (int): The depth of the search tree.
+        maximizingPlayer (bool): Whether the current agent is maximizing or minimizing.
+        alpha (float): The best value that the maximizing agent currently can guarantee at that level or above.
+        beta (float): The best value that the minimizing agent currently can guarantee at that level or above.
+        start_time (float): The time when the search started.
+        current_agent_id (int): The id of the current agent.
+
+        Returns:
+        score (float): The score of the current state.
+        """
+        # return None if timeout
+        if time.time() - start_time > THINKTIME:
+            return None
         
         if depth == 0 or not state.TilesRemaining():
-            # Return the heuristic value (state score)
             print(f'Agent {current_agent_id} reached depth limit, and the score is {ScoreState(state.agents[current_agent_id])}')
+            # the difference between the scores of our agent and the opponent
             return ScoreState(state.agents[self.id]) - ScoreState(state.agents[(self.id + 1) % NUM_PLAYERS])
 
         legal_actions = self.game_rule.getLegalActions(state, current_agent_id)
@@ -103,41 +126,50 @@ class myAgent:
 
         if maximizingPlayer:
             print(f'Agent {current_agent_id} is maximizing...')
-            maxEval = float('-inf')
+            maxScore = float('-inf')
             for action in legal_actions:
                 next_state = deepcopy(state)
+                # generate successors with current agent id
                 next_state = self.game_rule.generateSuccessor(next_state, action, current_agent_id)
-                eval = self.minimax(next_state, depth - 1, False, alpha, beta, start_time, think_time, (current_agent_id + 1) % NUM_PLAYERS)
+                score = self.minimax(next_state, depth - 1, False, alpha, beta, start_time, (current_agent_id + 1) % NUM_PLAYERS)
 
-                if eval is None:  # If time runs out, stop further evaluation
-                    return None
+                if score is None:
+                    if maxScore == float('-inf'):
+                        return None
+                    else:
+                        return maxScore
 
-                maxEval = max(maxEval, eval)
-                alpha = max(alpha, eval)
+                maxScore = max(maxScore, score)
+                alpha = max(alpha, score)
+                # alpha-beta pruning for maximizing
                 if beta <= alpha:
-                    break  # Alpha-beta pruning
-            return maxEval
+                    break
+            return maxScore
         else:
             print(f'Agent {current_agent_id} is minimizing...')
-            minEval = float('inf')
+            minScore = float('inf')
                 
             for action in legal_actions:
                 next_state = deepcopy(state)
+                # generate successors with current agent id
                 next_state = self.game_rule.generateSuccessor(next_state, action, current_agent_id)
-                eval = self.minimax(next_state, depth - 1, True, alpha, beta, start_time, think_time, (current_agent_id + 1) % NUM_PLAYERS)
+                score = self.minimax(next_state, depth - 1, True, alpha, beta, start_time, (current_agent_id + 1) % NUM_PLAYERS)
 
-                if eval is None:  # If time runs out, stop further evaluation
-                    return None
+                if score is None:
+                    if minScore == float('inf'):
+                        return None
+                    else:
+                        return minScore
 
-                minEval = min(minEval, eval)
-                beta = min(beta, eval)
+                minScore = min(minScore, score)
+                beta = min(beta, score)
+                # alpha-beta pruning for minimizing
                 if beta <= alpha:
-                    break  # Alpha-beta pruning
-            return minEval
+                    break
+            return minScore
 
     def SelectAction(self, actions, rootstate):
         start_time = time.time()
-        think_time = THINKTIME  # Defined global think time limit
         depth = 2
         best_action = None
         best_value = float('-inf')
@@ -146,34 +178,48 @@ class myAgent:
 
         print(f'Agent {self.id} is thinking...')
 
-        while time.time() - start_time < think_time:
-            current_best_action = None
-            current_best_value = float('-inf')
+        while time.time() - start_time < THINKTIME:
 
-            # Iterate through each legal action for the current depth
+            # at most search to depth 4
+            # to prevent timeout
+            # at the beginning of each round, the number of actions is large
+            # which limits the depth
+            # most of the time, the minimax stops at depth 3
+            if (depth == 4):
+                break
+
+            best_action_current_depth = None
+            # for our agent, we want to maximize the score
+            best_value_current_depth = float('-inf')
+
             for action in actions:
                 next_state = deepcopy(rootstate)
                 next_state = self.game_rule.generateSuccessor(next_state, action, self.id)
-                
-                # Call the minimax function for the opponent (minimizing player)
-                value = self.minimax(next_state, depth, False, alpha, beta, start_time, think_time, (self.id + 1) % NUM_PLAYERS)
+                print(f'Action: {action}')
+                # minimize the opponent's score
+                # the id should be the opponent's id
+                # because current agent already has successors generated
+                value = self.minimax(next_state, depth, False, alpha, beta, start_time, (self.id + 1) % NUM_PLAYERS)
 
-                if value is None:  # Timeout occurred
+                # no value returned when timeout
+                if value is None:
                     break
 
-                # Keep track of the best move found at this depth
-                if value > current_best_value:
-                    current_best_value = value
-                    current_best_action = action
+                if value > best_value_current_depth:
+                    best_value_current_depth = value
+                    best_action_current_depth = action
 
-            # If we have a valid action for this depth, update the best action
-            if current_best_action is not None:
-                best_action = current_best_action
-                best_value = current_best_value
+            # update best action and value if current depth is better
+            if best_action_current_depth is not None:
+                # coz there may be timeout
+                # the value returned may be suboptimal
+                if best_value_current_depth > best_value:
+                    best_action = best_action_current_depth
+                    best_value = best_value_current_depth
 
             # Dynamically increase depth if time allows
             depth += 1
 
         # exit()
-        # Return the best action found within the time limit
+        print(depth)
         return best_action if best_action is not None else random.choice(actions)
